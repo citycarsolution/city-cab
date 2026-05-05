e35fa1ba58b3828045dffb496d38e05ef95ad579
Bhupendra <codewithmukesh20025@gmail.com>
2026-05-05 19:39:14 +0530

    final map + autocomplete fix

--- app/components/MapView.tsx
+++ app/components/MapView.tsx
@@ -1,6 +1,19 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// 🔥 FIX MARKER ICON (VERY IMPORTANT)
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
 
 type Props = {
   from: { lat: number; lon: number };
@@ -18,17 +31,24 @@ export default function MapView({ from, to }: Props) {
   ];
 
   return (
-    <MapContainer
-      center={center}
-      zoom={7}
-      style={{ height: "300px", width: "100%", borderRadius: "10px" }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    // 🔥 FINAL WRAPPER FIX
+    <div className="map-wrapper mt-4">
+      <MapContainer
+        center={center}
+        zoom={7}
+        scrollWheelZoom={true}
+        className="w-full h-full"
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap"
+        />
 
-      <Marker position={center} />
-      <Marker position={[to.lat, to.lon]} />
+        <Marker position={center} />
+        <Marker position={[to.lat, to.lon]} />
 
-      <Polyline positions={route} color="blue" />
-    </MapContainer>
+        <Polyline positions={route} color="blue" />
+      </MapContainer>
+    </div>
   );
 }
